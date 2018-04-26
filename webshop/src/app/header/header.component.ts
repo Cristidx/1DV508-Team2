35f92c0b4d1a17e1e46933e8ed14d576d3d1cf85
Albert <alsetema@gmail.com>
2018-04-26 16:17:00 +0200

    fixed buttons showing only for admin and margins after pulling from old master

--- webshop/src/app/header/header.component.ts
+++ webshop/src/app/header/header.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { DataCloudService } from '../services/data-cloud.service';
-import {categoriesData} from '../model/data';
-import {AddCategoryComponent} from '../add-category/add-category.component'
+import { categoriesData } from '../model/data';
+import { AddCategoryComponent } from '../add-category/add-category.component'
 
 @Component({
   selector: 'app-header',
