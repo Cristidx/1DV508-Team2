1149dd2fe4560d1eee7743461dd4b2a77d3d6f93
eyuel <eyueltb@gmail.com>
2018-04-26 18:18:48 +0200

    data-cloud serve updated for delete movie and delete category

--- webshop/src/app/services/data-cloud.service.ts
+++ webshop/src/app/services/data-cloud.service.ts
@@ -1,25 +1,27 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
-import {movieData} from '../model/data';
-import {categoriesData} from '../model/data';
+import { movieData } from '../model/data';
+import { categoriesData } from '../model/data';
 
 @Injectable()
 export class DataCloudService {
 
   categoriesCollection: AngularFirestoreCollection<categoriesData>
   categoriesData: Observable<categoriesData[]>
+  categoryDoc: AngularFirestoreDocument<categoriesData>;
 
   movieCollection: AngularFirestoreCollection<movieData>
   movieData: Observable<movieData[]>
-
+  movieDoc: AngularFirestoreDocument<movieData>;
+  
   constructor(public afs: AngularFirestore) {
 
     //this.movieData = this.afs.collection('Movies').valueChanges();
     this.movieCollection = this.afs.collection('Movies');
     this.movieData = this.afs.collection('Movies').snapshotChanges().map(changes => {
       return changes.map(a => {
-        const data=a.payload.doc.data() as movieData;
+        const data = a.payload.doc.data() as movieData;
         data.id = a.payload.doc.id;
         return data;
       });
@@ -30,26 +32,35 @@ export class DataCloudService {
     this.categoriesCollection = this.afs.collection('Categories');
     this.categoriesData = this.afs.collection('Categories').snapshotChanges().map(changes => {
       return changes.map(a => {
-        const data=a.payload.doc.data() as categoriesData;
+        const data = a.payload.doc.data() as categoriesData;
         data.id = a.payload.doc.id;
         return data;
       });
     });
-   }
+  }
 
-  
-   getCategories(){
-    return this.categoriesData;  
+
+  getCategories() {
+    return this.categoriesData;
   }
-  getMovie(){
-    return this.movieData;  
+  getMovie() {
+    return this.movieData;
   }
-  addProduct(movieData: movieData){
+  addProduct(movieData: movieData) {
     this.movieCollection.add(movieData);
   }
-  addCategory(categoriesData: categoriesData){
-    this. categoriesCollection.add(categoriesData);
+  addCategory(categoriesData: categoriesData) {
+    this.categoriesCollection.add(categoriesData);
 
+  }
+   /* To delete categories */
+  deleteCategory(categoriesData: categoriesData) {
+    this.categoryDoc=this.afs.doc(`Categories/${categoriesData.id}`);
+  }
+  /* To delete movies item by id */
+  deleteMovie(movieData: movieData) {
+    this.movieDoc = this.afs.doc(`Movies/${movieData.id}`);
+    this.movieDoc.delete();
   }
 }
 
