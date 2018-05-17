65c254119a29373c75df9701808fe7620f3bb778
RSvensson98 <32928756+RSvensson98@users.noreply.github.com>
2018-05-17 16:26:00 +0200

    Update

--- webshop/src/app/profile/profile.component.ts
+++ webshop/src/app/profile/profile.component.ts
@@ -1,29 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { OrderService } from '../services/order.service';
+import { DataService } from '../services/data.service';
+import { Order } from '../model/order';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [OrderService]
 })
 export class ProfileComponent implements OnInit {
 
   showsOrders = false;
-
-  constructor(private authService: AuthService, private router: Router, private dialog: MatDialog) { }
+  uid: string;
+  orders: Order[];
+  currentlistCheck: boolean = false;
+  
+  constructor(private authService: AuthService, private router: Router, private data: DataService, private orderService: OrderService) { }
 
   ngOnInit() {
+	this.data.getavgRating(this.currentlistCheck,);
   }
 
   viewOrders(): void {
 	this.showsOrders = true;
+	this.getOrders();
   }
   
   hideOrders(): void {
 	this.showsOrders = false;
+  }
+  
+  getOrders(): void {
+	this.uid = this.authService.getUid();
+	this.orders = this.orderService.getOrdersByUid(this.uid);
   }
 
 }
