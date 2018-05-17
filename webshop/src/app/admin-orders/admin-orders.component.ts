eeebfe508a062c29d93f9b8a6eb9302c4f803fb5
cristidx <cristian.anchidin@gmail.com>
2018-05-17 18:59:12 +0200

    some stuf

--- webshop/src/app/admin-orders/admin-orders.component.ts
+++ webshop/src/app/admin-orders/admin-orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from '../model/order';
 import { DataCloudService } from '../services/data-cloud.service';
+import { DataService } from '../services/data.service';
 
 
 @Component({
@@ -12,12 +13,16 @@ export class AdminOrdersComponent implements OnInit {
   orders: Order[];
   movieOders:Order;
 
-  constructor(private dataService: DataCloudService) { }
+  currentlistCheck: boolean = false;
+  constructor(private dataService: DataCloudService, private data: DataService) { }
+
 
   ngOnInit() {
     this.dataService.getOrders().subscribe( movieOrders => {
       this.orders = movieOrders ;
     });
+
+    this.data.getavgRating(this.currentlistCheck);
   }
 
 }
