b3371ec188440d91ca8e4dbc54e9854d939b5e66
samuel <sbackstrom95@gmail.com>
2018-05-31 09:32:05 +0200

    added names to the order details page

--- webshop/src/app/order-details/order-details.component.ts
+++ webshop/src/app/order-details/order-details.component.ts
@@ -6,6 +6,8 @@ import { DataCloudService } from '../services/data-cloud.service';
 import { Order, Status } from '../model/order';
 import { Address } from '../model/address';
 import { element } from 'protractor';
+import { User } from '../model/user';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-order-details',
@@ -14,62 +16,62 @@ import { element } from 'protractor';
   providers: [OrderService]
 })
 export class OrderDetailsComponent implements OnInit {
-  
   id: string;
   orderDate: string;
   addres: Address;
   price: number;
   item: any[];
-  order: Order;/* to modify the oder */
+  order: Order; /* to modify the oder */
   orders: Order[];
+  user: User;
+  status: Status;
   uid: string;
-  status:Status;
   constructor(
-    private orderService: OrderService, 
-    private dataService: DataService, 
-    private cloudService: DataCloudService
-    ) {  }
+    private orderService: OrderService,
+    private dataService: DataService,
+    private cloudService: DataCloudService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.dataService.currentOrderUid.subscribe(uid => {
-      console.log(uid);
-      this.uid = uid;
+      this.authService.getUserByUid(uid).then(data => {
+        this.user = data;
+        this.orderService
+        .getOrdersByUid(this.user.uid)
+        .then(orders => {
+          this.orders = orders;
+        })
+        .catch(error => console.log(error));
+      });
     });
-    this.orderService.getOrdersByUid(this.uid)
-      .then((orders) => {
-        this.orders = orders;
-      })
-      .catch((error) => console.log(error));
-     
+
   }
 
   onSubmit() {
-   
-   this.orders.forEach(element => {
-       this.uid=element.uid;
-      this.orderDate=element.orderDate;
-      this.addres=element.address;
-      this.item=element.items;
-      this.status=element.status;
-      this.price=element.price;
-      this.id=element.id;
-      this.order={
-        uid:this.uid,
-        orderDate:this.orderDate,
-        address:this.addres,
-        items:this.item,
-        status:this.status,
-        price:this.price,
-        id:this.id
+    this.orders.forEach(element => {
+      this.uid = element.uid;
+      this.orderDate = element.orderDate;
+      this.addres = element.address;
+      this.item = element.items;
+      this.status = element.status;
+      this.price = element.price;
+      this.id = element.id;
+      this.order = {
+        uid: this.uid,
+        orderDate: this.orderDate,
+        address: this.addres,
+        items: this.item,
+        status: this.status,
+        price: this.price,
+        id: this.id
       };
       this.orderService.editOrder(this.order);
-      });
-      
-  
-   console.log(this.order.id);
-   console.log(this.id);
-   console.log(this.orders);
-   console.log(this.order);
-  
+    });
+
+    console.log(this.order.id);
+    console.log(this.id);
+    console.log(this.orders);
+    console.log(this.order);
   }
 }
--- webshop/src/app/services/auth.service.ts
+++ webshop/src/app/services/auth.service.ts
@@ -5,7 +5,10 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
 import { User } from '../model/user';
-import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from 'angularfire2/firestore';
 import { query } from '@angular/animations';
 import { Address } from '../model/address';
 import { DocumentReference } from '@firebase/firestore-types';
@@ -15,7 +18,11 @@ export class AuthService {
   user: Observable<User>;
   uid: string;
 
-  constructor(private afAuth: AngularFireAuth, private db: AngularFirestore, private router: Router) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router
+  ) {
     this.user = this.afAuth.authState.switchMap(user => {
       if (user != null) {
         this.uid = user.uid;
@@ -37,14 +44,13 @@ export class AuthService {
   }
 
   private oAuthLogin(provider) {
-    return this.afAuth.auth.signInWithPopup(provider)
-      .then((credential) => {
-        this.createUserDocument(credential.user);
-      });
+    return this.afAuth.auth.signInWithPopup(provider).then(credential => {
+      this.createUserDocument(credential.user);
+    });
   }
 
   saveUserAddress(address: Address) {
-    const userRef =  this.getUserRef();
+    const userRef = this.getUserRef();
     return userRef.update(address);
   }
 
@@ -53,9 +59,9 @@ export class AuthService {
     city: '',
     zipCode: '',
     phoneNumber: ''
-  }
+  };
   setUserAddressEmpty() {
-    const userRef =  this.getUserRef();
+    const userRef = this.getUserRef();
     return userRef.update(this.emptyAddress);
   }
 
@@ -64,9 +70,8 @@ export class AuthService {
   }
 
   signInAsAdmin(email: string, password: string) {
-    this.signInWithRegularEmail(email, password)
-    .then(() => {
-      this.user.subscribe((val) => {
+    this.signInWithRegularEmail(email, password).then(() => {
+      this.user.subscribe(val => {
         if (val.admin) {
           this.router.navigateByUrl('');
         } else {
@@ -77,27 +82,39 @@ export class AuthService {
   }
 
   createAccountWithRegularEmail(email: string, password: string) {
-    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
-        .then((user) => this.createUserDocument(user))
-        .catch((error) => console.log(error));
+    return this.afAuth.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(user => this.createUserDocument(user))
+      .catch(error => console.log(error));
   }
 
   getUserRef() {
     return this.db.firestore.doc(`Users/${this.uid}`);
   }
 
+  async getUserByUid(uid: string) {
+    let user;
+    let docRef = this.db.firestore.doc(`Users/${uid}`);
+    return new Promise(resolve => {
+      docRef.get().then(object => {
+        user = object.data();
+        resolve(user);
+      });
+    });
+  }
+
   createUserDocument(user) {
     const userRef = this.getUserRef();
-      return userRef.get().then((value) => {
-        if (!value.exists) {
-          const data: User = {
-            name: user.displayName,
-            email: user.email,
-            uid: user.uid,
-            admin: false
-          };
-          return userRef.set(data);
-        }
+    return userRef.get().then(value => {
+      if (!value.exists) {
+        const data: User = {
+          name: user.displayName,
+          email: user.email,
+          uid: user.uid,
+          admin: false
+        };
+        return userRef.set(data);
+      }
     });
   }
 
